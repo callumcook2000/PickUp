e5657f756d0c86b3f2a44ea95b4f3e93f42fc144
callum <W9083319@live.tees.ac.uk>
2021-09-06 02:36:18 +0100

    Add DB Connection Strings

--- app/src/main/java/uk/ac/tees/cis2003/W9083319/MainActivity.java
+++ app/src/main/java/uk/ac/tees/cis2003/W9083319/MainActivity.java
@@ -12,8 +12,12 @@ import java.lang.ClassNotFoundException;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Connection con;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public Connection connectionclass()
     {
+
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         Connection connection = null;
@@ -29,14 +34,45 @@ public class MainActivity extends AppCompatActivity {
         try
         {
             Class.forName("net.sourceforge.jtds.jdbc.Driver");
-            ConnectionURL = "jdbc:jtds:sqlserver://pick-up-app.database.windows.net:1433;databaseName=User;user=w9083319@pick-up-app;password=Cal1ben2@aron345;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            ConnectionURL = "jdbc:jtds:sqlserver://pick-up-app.database.windows.net:1433;DatabaseName=User;user=w9083319@pick-up-app;password=;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;ssl=request;";
             connection = DriverManager.getConnection(ConnectionURL);
         }
         catch (SQLException | ClassNotFoundException se)
         {
             Log.e("error:", se.getMessage());
+
+
         }
+
         return connection;
+    }
+
+    public void btnclick(View view)
+    {
+
+        TextView textView = (TextView)findViewById(R.id.textView1);
+
+
+        try{
+            con = connectionclass();
+            if (con == null)
+            {
+                textView.setText("con is null");
+            }
+            else
+            {
+                String query = "select * from User";
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                textView.setText("Success");
+
+            }
+        }
+        catch ( Exception ex)
+        {
+            Log.e("error:", ex.getMessage());
+        }
+
     }
 
 }
